51314779bda25e8446d18e5f1d58b99a2045346e
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2023-02-01 14:00:45 +0000

    done html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +7,25 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+ // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  console.log(episodeList);
+  for(const episode of episodeList) {
+  
+    const div = document.createElement('div')
+    const image = document.createElement('img')
+    image.src = episode.image.medium
+    div.appendChild(image)
+    div.innerHTML+= `${episode.name}, ${episode.season}, ${episode.number}`
+    rootElem.appendChild(div);
+    const season = 1;
+    const episode = 10;
+    season.episode+= `S${season.toString().padStart(2, 0)}E${episode}`
+
+    }
+
+
+    }
+
+
 
 window.onload = setup;
