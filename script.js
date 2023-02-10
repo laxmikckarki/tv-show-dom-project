527b41324d9d2028ddf0046fd0171c64aadfa069
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2023-02-10 21:59:17 +0000

    done level300

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //You can edit ALL of the code here
+const url = "https://api.tvmaze.com/shows/82/episodes";
+let allEpisodes = [];
 
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  fetch(url)
+  .then((res) => res.json())
+  .then((data) => { 
+    allEpisodes = data;
+  
   makePageForEpisodes(allEpisodes);
+})
+.catch((err) => console.error(err));
 }
 
 function makeSeasonAndEpisodes(episode) {
@@ -15,12 +23,16 @@ function makeSeasonAndEpisodes(episode) {
 
 function makePageForEpisodes(episodeList){
   const rootElem = document.getElementById("root");
+  const selectElem =document.getElementById('select-input');
   rootElem.innerHTML ="";
+
   const countParagraph = document.createElement('p');
   countParagraph.innerText = `Showing ${episodeList.length} episode`;
   rootElem.appendChild(countParagraph);
   episodeList.forEach((episode) => {
+    
     //add season and episode and name
+
     const paragraph = document.createElement('p');
     paragraph.textContent = `${makeSeasonAndEpisodes(episode)}: ${episode.name}`;
     rootElem.appendChild(paragraph);
@@ -33,25 +45,44 @@ function makePageForEpisodes(episodeList){
 
    // add the summary, episode summary
     rootElem.innerHTML+= episode.summary;
-  });
+ 
+
+// also, one more thing, add it to the select element as an option
+const option= document.createElement("option");
+option.textContent = `${makeSeasonAndEpisodes(episode)} - ${episode.name}`;
+option.value = episode.id;
+selectElem.appendChild(option);
+});
 }
 
 const searchInPut = document.getElementById('search-input');
 searchInPut.addEventListener('input', (event) => {
   const searchString = event.target.value.toLowerCase();
-  const filteredEpisode = getAllEpisodes().filter((episode) => {
+  const filteredEpisodes = allEpisodes.filter((episode) => {
 
       return (
         episode.summary.toLowerCase().includes(searchString) || 
         episode.name.toLowerCase().includes(searchString)
       );
     });
-  makePageForEpisodes(filteredEpisode);
+  makePageForEpisodes(filteredEpisodes);
+});
+
+const selectInPut = document.getElementById('select-input');
+selectInPut.addEventListener('change', (event) => {
+  
+ //the episode id that has been clicked on 
+  const idSelectByUser = Number(event.target.value);
+  const selectedEpisode = allEpisodes.find((episode) => episode.id === 
+  idSelectByUser);
+  if (selectedEpisode){
+    makePageForEpisodes([selectedEpisode])
+  }
 });
 
 window.onload = setup;
-/*
-fetch(url)
+
+/*fetch(url)
 .then((res) => res.json())
 .then((data)=>console.log(data))
 console.log('from the promise chain');
